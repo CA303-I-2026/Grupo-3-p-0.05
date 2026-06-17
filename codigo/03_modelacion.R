8537cac3a98ead605cebb3e973fee34f25d2bf75
Jose Miguel Rodriguez Gomez <jose.rodriguezgomez@ucr.ac.cr>
2026-06-17 12:17:51 -0600

    feat: se agrega la prueba de la distribución weibull para la distribución mixta de los costos

--- codigo/03_modelacion.R
+++ codigo/03_modelacion.R
@@ -6,6 +6,24 @@
 library(readr)
 library(tidyverse)
 library(here)
+library(cowplot)
+
+
+paleta_pamplemousse <- c(
+  "#E56B78",
+  "#F29CA3",
+  "#F2C38F",
+  "#36B7B4",
+  "#1E9CC7",
+  "#253494"
+)
+
+estilo_grupo3 <- theme_cowplot(font_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5),
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 11)
+)
 
 
 datos_modificados <- read.csv(here("datos", "procesados", "datos_modificados.csv"))
@@ -56,8 +74,8 @@ ajuste_gamma <- fitdist(costos_intervalo, "gamma")
 # Comparación del AIC (Criterio de Información de Akaike)
 
 comparacion <- data.frame(
-  Districución = c("Lognormal", "Gamma"),
-  AIC = c(ajuste_lognormal$aic, ajuste_gamma$aic),
+  Distribución = c("Lognormal", "Gamma"),
+  AIC = c(ajuste_lognormal$aic, ajuste_gamma$aic)
 )
 
 # Se puede observar que el AIC  de la lognomal es de 211,339.9 mientras que el de la gamma es de 212,684.5 resultando en una diferencia de 1344.6, por lo que el ajuste de la lognormal es mucho mejor que el ajuste de la gamma, lo que además indica una mejor representación de los datos
@@ -129,8 +147,8 @@ ajuste_pareto <- fitdist(mayores_900, "pareto", start = list(shape = 1, scale = 
 ajuste_lognormal_mayores_900 <- fitdist(mayores_900, "lnorm")
 
 comparacion_mayor_900 <- data.frame(
-  Districución = c("Lognormal", "Pareto"),
-  AIC = c(ajuste_lognormal_mayores_900$aic, ajuste_pareto$aic),
+  Distribución = c("Lognormal", "Pareto"),
+  AIC = c(ajuste_lognormal_mayores_900$aic, ajuste_pareto$aic)
 )
 
 # Gráfico diagnóstico para la pareto:
@@ -189,3 +207,156 @@ datos_modificados %>%
   summarise(Frecuencia = n()) %>%
   mutate(Proporcion = round(Frecuencia / sum(Frecuencia), 4), # Dividir cada frecuencia por el total de observaciones y redondearlo a 4 decimales
          Porcentaje = Proporcion * 100)
+
+
+
+
+# Bitácora 4
+
+# Prueba de la distribución Weibull, para la distribución mixta de los costos de reclamos
+
+# Distribución para costos entre 0 y 900 (Sin incluir extremos):
+library(fitdistrplus)
+
+# Ajuste para la distribución Weibull:
+ajuste_weibull = fitdist(costos_intervalo, "weibull")
+
+comparacion_weibull_lognormal <- data.frame(
+  Distribución = c("Lognormal", "Weibull"),
+  AIC = c(ajuste_lognormal$aic, ajuste_weibull$aic)
+)
+
+# Se puede observar que el AIC  de la lognomal es de 211,339.9 mientras que el de la distribución Weibull es de 212,884.4 resultando en una diferencia de 1,544.5, por lo que el ajuste de la lognormal es mucho mejor que el ajuste de la Weibull, lo que además indica una mejor representación de los datos
+
+# Gráfico diagnóstico para la weibull:
+plot(ajuste_weibull)
+diagnostico_weibull <- plot(ajuste_weibull)
+
+
+# Comparación gráfica entre la distribución Lognormal y la Weibull para el intervalo ]0,900[
+valores_x_weibull <- seq(0.1, 900, length.out = 1000) # Generar 1000 números, empezando en 0.1 y terminando en 900, los números estarán distanciados por el mismo valor (Como ir de 1 en 1 o de 2 en 2)
+
+curvas_comparacion_weibull <- data.frame(
+  valor_x = rep(valores_x_weibull, times = 2), # Duplicar los números creados antes
+  densidad = c(
+    dlnorm(valores_x_weibull, meanlog = ajuste_lognormal$estimate[1], sdlog = ajuste_lognormal$estimate[2]), # Calcular la distribución de la lognormal evaluándola en los 1000 valores, usando la media y desviación calculada anteriormente con el ajuste
+    dweibull(valores_x_weibull, shape = ajuste_weibull$estimate[1], scale = ajuste_weibull$estimate[2])),
+  distribucion = factor(rep(c(paste0("Lognormal con AIC = ", round(ajuste_lognormal$aic, 0)), # Crear las etiquetas para mostrar la diferencia de AIC
+                              paste0("Weibull con AIC = ", round(ajuste_weibull$aic, 0))), 
+                            each = 1000)) # Repetir cada etiqueta 1000 veces, los primeros 1000 para la lognormal y los otros 1000 para la Weibull
+)
+
+# Gráfico para comparar la distribución de la Lognormal con la Weibull para el intervalo ]0,900[
+comparacion_lognormal_weibull <- datos_modificados %>%
+  filter(Cost_claims_year > 0 & Cost_claims_year < 900) %>% 
+  ggplot(aes(x = Cost_claims_year)) +
+  geom_histogram(aes(y = after_stat(density)), # Hacer que el alto total del histograma sea 1 (Representarlo como densidad)
+                 bins = 50, fill = "gray80", color = "white", alpha = 0.6) + # Color del histograma
+  geom_line(data = curvas_comparacion_weibull, aes(x = valor_x, y = densidad, color = distribucion), size = 1.2) + # Graficar la lognormal y la weibull y ponerlas sobre el histograma 
+  scale_color_manual(values = c(paleta_pamplemousse[1], paleta_pamplemousse[6])) + # Roja la Lognormal, azul la Weibull
+  labs(title = "Comparación del ajuste de la lognormal y la Weibull",
+       x = "Costo anual",
+       y = "Densidad",
+       color = "Distribución") +
+  scale_x_continuous(breaks = seq(0,900,100))+
+  estilo_grupo3
+
+
+
+# Comparación de las distribuciones Lognormal, Gamma y Weibull para el intervalo ]0,900[
+
+valores_x_weibull_gamma <- seq(0.1, 900, length.out = 1000) # Generar 1000 números, empezando en 0.1 y terminando en 900, los números estarán distanciados por el mismo valor (Como ir de 1 en 1 o de 2 en 2)
+
+curvas_comparacion_weibull_gamma <- data.frame(
+  valor_x = rep(valores_x_weibull_gamma, times = 3), # Repetir 3 los números creados antes
+  densidad = c(
+    dlnorm(valores_x_weibull_gamma, meanlog = ajuste_lognormal$estimate[1], sdlog = ajuste_lognormal$estimate[2]), # Calcular la distribución de la lognormal evaluándola en los 1000 valores, usando la media y desviación calculada anteriormente con el ajuste
+    dweibull(valores_x_weibull_gamma, shape = ajuste_weibull$estimate[1], scale = ajuste_weibull$estimate[2]),
+    dgamma(valores_x_weibull_gamma, shape = ajuste_gamma$estimate[1], rate = ajuste_gamma$estimate[2])),
+  distribucion = factor(rep(c(paste0("Lognormal con AIC = ", round(ajuste_lognormal$aic, 0)), # Crear las etiquetas para mostrar la diferencia de AIC
+                              paste0("Weibull con AIC = ", round(ajuste_weibull$aic, 0)),
+                              paste0("Gamma con AIC = ", round(ajuste_gamma$aic, 0))), 
+                            each = 1000)) # Repetir cada etiqueta 1000 veces, los primeros 1000 para la lognormal, los siguientes 1000 para la Weibull y los útimos 1000 para la Gamma
+)
+
+# Gráfico para comparar la distribución de la Lognormal, la Weibull y la Gamma para el intervalo ]0,900[
+comparacion_lognormal_weibull_gamma <- datos_modificados %>%
+  filter(Cost_claims_year > 0 & Cost_claims_year < 900) %>% 
+  ggplot(aes(x = Cost_claims_year)) +
+  geom_histogram(aes(y = after_stat(density)), # Hacer que el alto total del histograma sea 1 (Representarlo como densidad)
+                 bins = 50, fill = "gray80", color = "white", alpha = 0.6) + # Color del histograma
+  geom_line(data = curvas_comparacion_weibull_gamma, aes(x = valor_x, y = densidad, color = distribucion), size = 1.2) + # Graficar la lognormal y la weibull y ponerlas sobre el histograma 
+  scale_color_manual(values = c(paleta_pamplemousse[1], paleta_pamplemousse[3], paleta_pamplemousse[6])) + # Roja la Gamma, amarilla para la Lognormal y azul la Weibull
+  labs(title = "Comparación del ajuste de la distribución Lognormal, Gamma y Weibull",
+       x = "Costo anual",
+       y = "Densidad",
+       color = "Distribución") +
+  scale_x_continuous(breaks = seq(0,900,100))+
+  estilo_grupo3
+
+
+
+# Comparación entre la distribución Lognormal y la Weibull para el intervalo ]900,2000[
+ajuste_weibull_mayores_900 <- fitdist(mayores_900, "weibull")
+
+comparacion_mayor_900_weibull <- data.frame(
+  Distribución = c("Lognormal", "Weibull"),
+  AIC = c(ajuste_lognormal_mayores_900$aic, ajuste_weibull_mayores_900$aic)
+)
+
+curvas_comparacion_mayor_900_weibull <- data.frame(
+  valor_x = rep(valores_x_900, times = 2), # Duplicar los números creados antes
+  densidad = c(
+    dlnorm(valores_x_900, meanlog = ajuste_lognormal_mayores_900$estimate[1], sdlog = ajuste_lognormal_mayores_900$estimate[2]), # Calcular la distribución de la lognormal evaluándola en los 1000 valores, usando la media y desviación canculada anteriormente con el ajuste
+    dweibull(valores_x_900, shape = ajuste_weibull_mayores_900$estimate[1], scale = ajuste_weibull_mayores_900$estimate[2])),
+  distribucion = factor(rep(c(paste0("Lognormal con AIC = ", round(ajuste_lognormal_mayores_900$aic, 0)), # Crear las etiquetas para mostrar la diferencia de AIC
+                              paste0("Weibull con AIC = ", round(ajuste_weibull_mayores_900$aic, 0))), 
+                            each = 1000)) # Repetir cada etiqueta 1000 veces, los primeros 1000 para la lognormal y los otros 1000 para la Weibull
+)
+
+# Gráfico para comparar la distribución de la Lognormal con la Weibull para el intervalo [900, 20000[
+comparacion_lognormal_weibull_mayores_900 <- datos_modificados %>%
+  filter(Cost_claims_year >= 900 & Cost_claims_year <= 20000) %>% 
+  ggplot(aes(x = Cost_claims_year)) +
+  geom_histogram(aes(y = after_stat(density)), # Hacer que el alto total del histograma sea 1 (Representarlo como densidad)
+                 bins = 50, fill = "gray80", color = "white", alpha = 0.6) + # Color del histograma
+  geom_line(data = curvas_comparacion_mayor_900_weibull, aes(x = valor_x, y = densidad, color = distribucion), size = 1.2) + # Graficar la lognormal y la gamma y ponerlas sobre el histograma 
+  scale_color_manual(values = c(paleta_pamplemousse[1], paleta_pamplemousse[6])) + # Roja la Lognormal, azul la Weibull
+  labs(title = "Comparación del ajuste de la Distribución Lognormal y la Weibull",
+       x = "Costo anual",
+       y = "Densidad",
+       color = "Distribución") +
+  scale_x_continuous(breaks = seq(900,20000,1800))+
+  estilo_grupo3
+
+
+
+# Comparación de las distribuciones Lognormal, Pareto y Weibull para el intervalo [900, 20000[
+
+curvas_comparacion_mayor_900_weibull_pareto <- data.frame(
+  valor_x = rep(valores_x_900, times = 3), # Repetir 3 los números creados antes
+  densidad = c(
+    dlnorm(valores_x_900, meanlog = ajuste_lognormal_mayores_900$estimate[1], sdlog = ajuste_lognormal_mayores_900$estimate[2]), # Calcular la distribución de la lognormal evaluándola en los 1000 valores, usando la media y desviación canculada anteriormente con el ajuste
+    dweibull(valores_x_900, shape = ajuste_weibull_mayores_900$estimate[1], scale = ajuste_weibull_mayores_900$estimate[2]),
+    dpareto(valores_x_900, shape = ajuste_pareto$estimate[1], scale = ajuste_pareto$estimate[2])),
+  distribucion = factor(rep(c(paste0("Lognormal con AIC = ", round(ajuste_lognormal_mayores_900$aic, 0)), # Crear las etiquetas para mostrar la diferencia de AIC
+                              paste0("Weibull con AIC = ", round(ajuste_weibull_mayores_900$aic, 0)),
+                              paste0("Pareto con AIC = ", round(ajuste_pareto$aic, 0))), 
+                            each = 1000)) # Repetir cada etiqueta 1000 veces, los primeros 1000 para la lognormal, los siguientes 1000 para la Weibull y lo últimos 1000 para la Pareto
+)
+
+# Gráfico para comparar la distribución de la Lognormal, la Pareto y la Weibull para el intervalo [900, 20000[
+comparacion_lognormal_weibull_mayores_pareto_900 <- datos_modificados %>%
+  filter(Cost_claims_year >= 900 & Cost_claims_year <= 20000) %>% 
+  ggplot(aes(x = Cost_claims_year)) +
+  geom_histogram(aes(y = after_stat(density)), # Hacer que el alto total del histograma sea 1 (Representarlo como densidad)
+                 bins = 50, fill = "gray80", color = "white", alpha = 0.6) + # Color del histograma
+  geom_line(data = curvas_comparacion_mayor_900_weibull_pareto, aes(x = valor_x, y = densidad, color = distribucion), size = 1.2) + # Graficar la lognormal y la gamma y ponerlas sobre el histograma 
+  scale_color_manual(values = c(paleta_pamplemousse[3], paleta_pamplemousse[1], paleta_pamplemousse[6])) + # Roja la Lognormal, azul la Weibull
+  labs(title = "Comparación del ajuste de la Distribución Lognormal, Pareto y Weibull",
+       x = "Costo anual",
+       y = "Densidad",
+       color = "Distribución") +
+  scale_x_continuous(breaks = seq(900,20000,1800)) +
+  scale_y_continuous(labels = scales::comma, breaks = seq(0,0.0007, 0.0001)) +
+  estilo_grupo3
